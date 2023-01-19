6f0ee6a123ea637d4027d5a41fd55e4c5d17aef3
nnajeh <38373885+nnajeh@users.noreply.github.com>
2023-01-19 18:46:30 +0100

    Create Spectral_normalization.py

---
+++ utils/Spectral_normalization.py
@@ -0,0 +1,206 @@
+from Libraries import *
+
+
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch import nn
+from torch import Tensor
+from torch.nn import Parameter
+
+def l2normalize(v, eps=1e-12):
+    return v / (v.norm() + eps)
+
+
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = l2normalize(torch.mv(torch.t(w.view(height,-1).data), u.data))
+            u.data = l2normalize(torch.mv(w.view(height,-1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2normalize(u.data)
+        v.data = l2normalize(v.data)
+        w_bar = Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
+
+
+
+
+import os
+import torch
+import torch.nn as nn
+from torch import autograd, optim
+from torch.utils.data import DataLoader, Dataset
+
+import torchvision
+from torchvision import transforms
+from torchvision.utils import save_image
+
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import torchvision.datasets as dataset
+import torchvision.datasets 
+
+
+
+# Projection of x onto y
+def proj(x, y):
+  return torch.mm(y, x.t()) * y / torch.mm(y, y.t())
+
+
+# Orthogonalize x wrt list of vectors ys
+def gram_schmidt(x, ys):
+  for y in ys:
+    x = x - proj(x, y)
+  return x
+
+
+# Apply num_itrs steps of the power method to estimate top N singular values.
+def power_iteration(W, u_, update=True, eps=1e-12):
+  # Lists holding singular vectors and values
+  us, vs, svs = [], [], []
+  for i, u in enumerate(u_):
+    # Run one step of the power iteration
+    with torch.no_grad():
+      v = torch.matmul(u, W)
+      # Run Gram-Schmidt to subtract components of all other singular vectors
+      v = F.normalize(gram_schmidt(v, vs), eps=eps)
+      # Add to the list
+      vs += [v]
+      # Update the other singular vector
+      u = torch.matmul(v, W.t())
+      # Run Gram-Schmidt to subtract components of all other singular vectors
+      u = F.normalize(gram_schmidt(u, us), eps=eps)
+      # Add to the list
+      us += [u]
+      if update:
+        u_[i][:] = u
+    # Compute this singular value and add it to the list
+    svs += [torch.squeeze(torch.matmul(torch.matmul(v, W.t()), u.t()))]
+    #svs += [torch.sum(F.linear(u, W.transpose(0, 1)) * v)]
+  return svs, us, vs
+
+
+
+# Convenience passthrough function
+class identity(nn.Module):
+  def forward(self, input):
+    return input
+    
+    
+
+# Spectral normalization base class 
+class SN(object):
+  def __init__(self, num_svs, num_itrs, num_outputs, transpose=False, eps=1e-12):
+    # Number of power iterations per step
+    self.num_itrs = num_itrs
+    # Number of singular values
+    self.num_svs = num_svs
+    # Transposed?
+    self.transpose = transpose
+    # Epsilon value for avoiding divide-by-0
+    self.eps = eps
+    # Register a singular vector for each sv
+    for i in range(self.num_svs):
+      self.register_buffer('u%d' % i, torch.randn(1, num_outputs))
+      self.register_buffer('sv%d' % i, torch.ones(1))
+  
+  # Singular vectors (u side)
+  @property
+  def u(self):
+    return [getattr(self, 'u%d' % i) for i in range(self.num_svs)]
+
+  # Singular values; 
+  # note that these buffers are just for logging and are not used in training. 
+  @property
+  def sv(self):
+   return [getattr(self, 'sv%d' % i) for i in range(self.num_svs)]
+   
+  # Compute the spectrally-normalized weight
+  def W_(self):
+    W_mat = self.weight.view(self.weight.size(0), -1)
+    if self.transpose:
+      W_mat = W_mat.t()
+    # Apply num_itrs power iterations
+    for _ in range(self.num_itrs):
+      svs, us, vs = power_iteration(W_mat, self.u, update=self.training, eps=self.eps) 
+    # Update the svs
+    if self.training:
+      with torch.no_grad(): # Make sure to do this in a no_grad() context or you'll get memory leaks!
+        for i, sv in enumerate(svs):
+          self.sv[i][:] = sv     
+    return self.weight / svs[0]
+
+
+# 2D Conv layer with spectral norm
+class SNConv2d(nn.Conv2d, SN):
+  def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, 
+               num_svs=1, num_itrs=1, eps=1e-12):
+    
+    nn.Conv2d.__init__(self, in_channels, out_channels, kernel_size, stride,  padding, dilation, groups, bias)
+
+    SN.__init__(self, num_svs, num_itrs, out_channels, eps=eps)
+
+  def forward(self, x):
+    return F.conv2d(x, self.W_(), self.bias, self.stride,  self.padding, self.dilation, self.groups)
+
+
+
+# Linear layer with spectral norm
+class SNLinear(nn.Linear, SN):
+  def __init__(self, in_features, out_features, bias=True,
+               num_svs=1, num_itrs=1, eps=1e-12):
+    nn.Linear.__init__(self, in_features, out_features, bias)
+    SN.__init__(self, num_svs, num_itrs, out_features, eps=eps)
+  def forward(self, x):
+    return F.linear(x, self.W_(), self.bias)
+
