a068db96dea3cc423aa888abd2d5a6fea1fc07f0
nnajeh <38373885+nnajeh@users.noreply.github.com>
2023-01-20 23:27:54 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,74 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import cv2
+import os
+
+
+# import the necessary packages
+def prepare_plot(origImage, origMask, predMask):
+	# initialize our figure
+	figure, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
+	# plot the original image, its mask, and the predicted mask
+	ax[0].imshow(origImage[0][0])
+	ax[1].imshow(origMask[0][0])
+	ax[2].imshow(predMask[0][0].cpu().data.numpy())
+	# set the titles of the subplots
+	ax[0].set_title("Image")
+	ax[1].set_title("Original Mask")
+	ax[2].set_title("Predicted Mask")
+	# set the layout of the figure and display it
+	figure.tight_layout()
+	figure.show()
+  
+
+for i, (imgs,labels, masks) in enumerate(test_dataloader):   
+    
+        real_img = imgs.to(device)
+        mask = masks.to('cuda')
+        fake_imgs = pretrained_G(pretrained_E(real_img))
+    
+                
+        #encoder Output
+        real_img_feat,_ = pretrained_D.extract_all_features(real_img)
+        fake_img_feat,_ = pretrained_D.extract_all_features(fake_imgs)
+                
+    
+        ### Decoder Output
+        _,real_pixel_feat = pretrained_D.extract_all_features(real_img)
+        _,fake_pixel_feat = pretrained_D.extract_all_features(fake_imgs)
+        
+        #print(real_img_feat[0])
+ 
+       # feat_distance1 = MSE(f_gx_e, f_x_e)
+        anomaly_score=  torch.abs(fake_imgs-real_img)+ torch.mean(torch.abs(fake_pixel_feat - real_pixel_feat))  
+        
+        # make the prediction, pass the results through the sigmoid
+        # function, and convert the result to a NumPy array
+        predMask = anomaly_score.squeeze()
+        predMask = torch.sigmoid(predMask)
+        predMask = predMask.cpu().detach().numpy()
+        
+        # filter out the weak predictions and convert them to integers
+        predMask = (predMask > 0.5) * 255
+        predMask = predMask.astype(np.uint8)
+    
+
+
+        #plt.show()
+            
+        visualize(
+           original_image = real_img[0][0].cpu().data.numpy(),
+           ground_truth_mask = masks[0][0].cpu().data.numpy(),
+           predicted_mask_real = anomaly_score[0][0].cpu().data.numpy(),
+           predicted_mask_fake = fake_pixel_feat[0][0].cpu().data.numpy(),
+           outputs = (torch.argmax(torch.softmax(anomaly_score, dim=1), dim=1, keepdim=True))[0][0].cpu().data.numpy(),
+           predMask = predMask[0],
+
+            #predicted_building_heatmap = pred_building_heatmap
+          )
+                
+        x =plt.imshow(anomaly_score[0][0].cpu().data.numpy(), extent=(-3, 3, 3, -3), interpolation='bilinear', cmap='jet')
+        plt.colorbar(x);
+
